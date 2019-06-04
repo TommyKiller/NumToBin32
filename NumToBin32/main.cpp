1c0dc5693904a6fd926f28a92e87306d87d482e8
Николай <Nicko6k9@yandex.ua>
2019-06-04 03:57:57 +0300

    Made beautiful

--- NumToBin32/main.cpp
+++ NumToBin32/main.cpp
@@ -1,48 +1,53 @@
 #include <bitset>
 #include <iostream>
 #include <cmath>
+#include <string>
+
+
+void FloatToBin(long double value)
+{
+	std::bitset<32> value32ieee754;
+	value32ieee754[31] = value < 0 ? 1 : 0;
+
+	float bs = log(abs(value)) / log(2);
+	bs = bs < 0 ? ceil(bs) : floor(bs);
+	int p = 127 + bs;
+	std::bitset<8> value_int_part(p);
+
+	int float_part_bits = 23 - bs;
+	long float_part = round((abs(value) - pow(2, bs)) * pow(2, float_part_bits));
+	std::bitset<23> value_float_part(float_part);
+
+	for (int i = 0; i < 31; i++)
+	{
+		value32ieee754[i] = i < 23 ? value_float_part[i] : value_int_part[i - 23];
+	}
+
+	printf(
+		"Input value: %f				\n\
+		bs: %.0f						\n\
+		p: %i							\n\
+		Integer part: %s				\n\
+		Float part = (%f - %.0f) * %.0f;\n\
+		Float part: %s					\n", value, bs, p, value_int_part.to_string().c_str(), value, pow(2, bs), pow(2, float_part_bits), value_float_part.to_string().c_str());
+
+	printf(
+		"\		Result: %s				\n\
+		Hex result: %X					\n", value32ieee754.to_string().c_str(), value32ieee754);
+
+}
 
 
 int main()
 {
-	while (true)
+	std::string input = "";
+	std::cin >> input;
+
+	while ((input != "exit") && (input != "q"))
 	{
-		long double value;
+		FloatToBin(atof(input.c_str()));
 
-		std::cin >> value;
-
-		std::bitset<32> value32_bit;
-
-		value32_bit[31] = value < 0 ? 1 : 0;
-
-		long double bs = log(abs(value)) / log(2);
-
-		bs = bs < 0 ? ceil(bs) : floor(bs);
-
-		std::cout << "bs = " << bs << std::endl;
-
-		long p = 127 + bs;
-
-		std::bitset<8> value_power(p);
-
-		std::cout << "Power = " << value_power << std::endl;
-
-		int float_bits = 23 - bs;
-
-		long float_part = round((abs(value) - pow(2, bs)) * pow(2, float_bits));
-
-		std::cout << "(" << value << " - " << pow(2, bs) << ") * " << pow (2, float_bits) << float_part << std::endl;
-
-		std::bitset<23> value_float_part(float_part);
-
-		std::cout << value_float_part << std::endl;
-
-		for (int i = 0; i < 31; i++)
-		{
-			value32_bit[i] = i < 23 ? value_float_part[i] : value_power[i - 23];
-		}
-
-		std::cout << "Result = " << value32_bit << std::endl;
+		std::cin >> input;
 	}
 
 	return 0;
