efa32dff28c7671efaa83461e786c21d27b0b45f
Николай <Nicko6k9@yandex.ua>
2019-06-04 21:14:27 +0300

    .

--- NumToBin32/main.cpp
+++ NumToBin32/main.cpp
@@ -23,30 +23,29 @@ void FloatToBin(long double value)
 		value32ieee754[i] = i < 23 ? value_float_part[i] : value_int_part[i - 23];
 	}
 
-	printf(
-		"Input value: %f				\n\
+	printf("\
 		bs: %.0f						\n\
 		p: %i							\n\
 		Integer part: %s				\n\
 		Float part = (%f - %.0f) * %.0f;\n\
-		Float part: %s					\n", value, bs, p, value_int_part.to_string().c_str(), value, pow(2, bs), pow(2, float_part_bits), value_float_part.to_string().c_str());
-
-	printf(
-		"\		Result: %s				\n\
-		Hex result: %X					\n", value32ieee754.to_string().c_str(), value32ieee754);
-
+		Float part: %s					\n\
+		Result: %s						\n\
+		Hex result: %X					\n", bs, p, value_int_part.to_string().c_str(), value, pow(2, bs), pow(2, float_part_bits),
+		value_float_part.to_string().c_str(), value32ieee754.to_string().c_str(), value32ieee754);
 }
 
 
 int main()
 {
-	std::string input = "";
+	std::string input;
+	printf("Input value: ");
 	std::cin >> input;
 
-	while ((input != "exit") && (input != "q"))
+	while (input != "exit" && input != "q")
 	{
 		FloatToBin(atof(input.c_str()));
 
+		printf("Input value: ");
 		std::cin >> input;
 	}
 
